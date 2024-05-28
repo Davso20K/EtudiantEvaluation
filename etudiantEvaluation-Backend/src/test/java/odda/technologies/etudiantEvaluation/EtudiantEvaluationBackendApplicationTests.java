b2a990b827f4e19a9890e73e7b08ceac222fd224
Frankstein AFOMALE <afomaledrank@gmail.com>
2024-05-28 11:07:41 +0000

    Projet initialisé

---
+++ etudiantEvaluation-Backend/src/test/java/odda/technologies/etudiantEvaluation/EtudiantEvaluationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package odda.technologies.etudiantEvaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiantEvaluationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
